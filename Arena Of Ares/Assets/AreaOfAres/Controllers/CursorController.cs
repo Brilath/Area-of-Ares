e2cb85cba3628c96cf25c1e6eb72af1bb5f85f0e
Brilath <Brilath@yahoo.com>
2020-05-17 22:34:18 -0400

    Custom Mouse Cursor

---
+++ Arena Of Ares/Assets/AreaOfAres/Controllers/CursorController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorController : MonoBehaviour
+{
+
+    [SerializeField] private Texture2D _cursorTexture;
+
+    private void Awake()
+    {
+        Cursor.SetCursor(_cursorTexture, new Vector2(10, 10), CursorMode.Auto);
+    }
+}
